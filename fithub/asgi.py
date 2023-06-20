fef4f98fff6c6b453a174657a1b574647b8efb21
shamila p <lulushamila@gmail.com>
2023-06-20 19:06:59 +0530

    updates

--- fithub/asgi.py
+++ fithub/asgi.py
@@ -4,7 +4,6 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from api.routing import websocket_urlpatterns
-from daphne.server import Server
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fithub.settings')
 import django
