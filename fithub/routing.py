a63dc015074788fbcf5c960871928c65f99fb7fe
shamila p <lulushamila@gmail.com>
2023-06-23 23:58:48 +0530

    routings.py

--- fithub/routing.py
+++ fithub/routing.py
@@ -3,6 +3,6 @@ from api import consumers
 
 
 websocket_urlpatterns = [
-    path('trainer/chat/<int:trainer_id>/<int:thread_id>/', consumers.ChatConsumer.as_asgi()),
-    path('user/chat/<int:trainer_id>/<int:thread_id>/', consumers.ChatConsumer.as_asgi())
+    path('ws/trainer/chat/<int:trainer_id>/<int:thread_id>/', consumers.ChatConsumer.as_asgi()),
+    path('ws/user/chat/<int:trainer_id>/<int:thread_id>/', consumers.ChatConsumer.as_asgi())
 ]
